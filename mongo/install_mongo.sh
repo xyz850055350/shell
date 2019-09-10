5d22bf933619f732e7eb951d166c41041550d6a0
xyz850055350 <51713610+xyz850055350@users.noreply.github.com>
2019-09-10 15:08:00 +0800

    Update install_mongo.sh

--- mongo/install_mongo.sh
+++ mongo/install_mongo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #mongodb单机版安装
-wget -P /usr/local/src http://10.10.10.10/package/mongo/mongodb-linux-x86_64-3.6.3.tgz
-#wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-4.0.3.tgz
+wget -P /usr/local/src http://10.10.10.10/package/mongo/mongodb-linux-x86_64-rhel70-4.2.0.tgz
+#wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-4.2.0.tgz
 wget -P /usr/local/src http://10.10.10.10/package/mongo/mongodb.conf
 wget -P /usr/local/src http://10.10.10.10/package/mongo/mongodb.service
 
 username=app
-file_source=/usr/local/src/mongodb-linux-x86_64-3.6.3.tgz
-mongo_ver=mongodb-linux-x86_64-3.6.3
+file_source=/usr/local/src/mongodb-linux-x86_64-rhel70-4.2.0.tgz
+mongo_ver=mongodb-linux-x86_64-rhel70-4.2.0
 
 if [ `whoami` != "root" ]; then
     echo "需使用root用户执行"
